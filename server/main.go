6da0f0d044f742f2346385d4f2df0926ca2732d9
Loïc <loic.collatuzo@ynov.com>
2024-11-19 13:23:26 +0100

    landing page

--- server/main.go
+++ server/main.go
@@ -12,6 +12,7 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	var fileName = "../templates/home.html"
+	gs.InitGame()
 	template.ParseFiles(fileName)
 	t, err := template.ParseFiles(fileName)
 	if err != nil {
@@ -19,13 +20,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data := gs.GameState {
-		Lives : gs.GetLives(),
-		Word : gs.GetRandomWord(),
-		Blanks: gs.GetBlanks(),
-	}
-
-	t.Execute(w, data)
+	t.Execute(w, nil)
 }
 
 func play(w http.ResponseWriter, r *http.Request) {
@@ -36,7 +31,17 @@ func play(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Erreur pendant le parsing", err)
 		return
 	}
-	t.Execute(w, nil)
+
+
+	data := gs.GameState {
+		Lives : gs.GetLives(),
+		Word : gs.GetWord(),
+		Blanks: gs.GetBlanks(),
+	}
+	println(data.Word)
+	println(data.Blanks)
+
+	t.Execute(w, data)
 }
 
 func handler(w http.ResponseWriter, r *http.Request){
